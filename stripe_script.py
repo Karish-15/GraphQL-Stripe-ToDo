fb5d4279992a2ca7b385965c971e431c80804e13
Karish Chaudhary <74651250+Karish-15@users.noreply.github.com>
2023-10-14 04:02:06 +0530

    Update stripe_script.py

--- stripe_script.py
+++ stripe_script.py
@@ -1,7 +1,1 @@
-import stripe
-stripe.api_key = "sk_test_51O0rdqSAx2prmQegmfQK9gsyHU0ki4XCNbbXEQXZ7pp01VFnhQlSRhnZ0UGZAhLlp49DMOIKfsjJPcRCsnbqXT4q00HMa64fF4"
-
-stripe.Price.create(
-    product ="prod_OoUl1nmBJUZOAS", unit_amount = 20, currency = "usd",
-)
 
